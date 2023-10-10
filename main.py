3ce33decc68ee8f775827f3be6bf6a752b2ceaef
Sanjik761 <sanjarburiniyozov@gmail.com>
2023-10-10 11:03:46 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+n = int(input("Enter your number: "))
+if n >= 0:
+    def countdown(n):
+        if n == 0:
+            print('Blastoff!')
+        else:
+            while n >= 0:
+                print(n)
+                if n == 0:
+                    print('Blastoff!')
+                    break
+                n -= 1
+
+
+    countdown(n)
+
+else:
+    def countup(n):
+        if n == 0:
+            print('Blastoff!')
+        else:
+            while n <= 0:
+                print(n)
+                if n == 0:
+                    print('Blastoff!')
+                    break
+                n += 1
+    countup(n)
+
